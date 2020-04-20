bcca8d33ef46092c1dae71eef4a47e5b49916cbb
钱程 <abcqdsdchina@126.com>
2020-04-20 21:56:16 +0800

    建立jenkins-app演示代码

---
+++ src/main/java/club/avence/spring/boot/Application.java
@@ -0,0 +1,18 @@
+package club.avence.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动类
+ *
+ * @author qian3
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
