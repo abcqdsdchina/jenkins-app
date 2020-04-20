4f98d3cb307f805b171b71a0bf409ea50d7563ec
钱程 <abcqdsdchina@126.com>
2020-04-20 21:56:59 +0800

    添加欢迎信息注释

--- src/main/java/club/avence/spring/boot/controller/HelloController.java
+++ src/main/java/club/avence/spring/boot/controller/HelloController.java
@@ -3,6 +3,11 @@ package club.avence.spring.boot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 欢迎信息
+ *
+ * @author qian3
+ */
 @RestController
 public class HelloController {
 
